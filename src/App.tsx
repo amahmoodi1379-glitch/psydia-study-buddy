2491526ec6e3e01e60a27c717442a5d4347ca1a4
amahmoodi1379-glitch <a.mahmoodi1379@gmail.com>
2025-12-23 18:33:03 +0330

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { themeStorage } from '@/lib/storage';
 import { initTelegramApp } from '@/lib/telegram';
+import AuthGate from "@/components/AuthGate";
 
 // Pages
 import ExamHome from "./pages/ExamHome";
@@ -45,28 +46,30 @@ const App = () => {
       <TooltipProvider>
         <Toaster />
         <Sonner position="top-center" />
-        <BrowserRouter>
-          <Routes>
-            {/* Exam tab routes */}
-            <Route path="/" element={<ExamHome />} />
-            <Route path="/subjects/:subjectId/topics" element={<Topics />} />
-            <Route path="/topics/:topicId/subtopics" element={<Subtopics />} />
-            <Route path="/hub/:subtopicId" element={<SubtopicHub />} />
-            <Route path="/player" element={<Player />} />
-            <Route path="/summary" element={<SessionSummary />} />
-            
-            {/* Profile tab routes */}
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/profile/settings" element={<Settings />} />
-            <Route path="/profile/stats" element={<Stats />} />
-            <Route path="/profile/stats/subject/:subjectId" element={<SubjectStats />} />
-            <Route path="/profile/heatmap" element={<Heatmap />} />
-            <Route path="/profile/bookmarks" element={<Bookmarks />} />
-            
-            {/* Catch-all */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+        <AuthGate>
+  <HashRouter>
+    <Routes>
+      {/* Exam tab routes */}
+      <Route path="/" element={<ExamHome />} />
+      <Route path="/subjects/:subjectId/topics" element={<Topics />} />
+      <Route path="/topics/:topicId/subtopics" element={<Subtopics />} />
+      <Route path="/hub/:subtopicId" element={<SubtopicHub />} />
+      <Route path="/player" element={<Player />} />
+      <Route path="/summary" element={<SessionSummary />} />
+
+      {/* Profile tab routes */}
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/profile/settings" element={<Settings />} />
+      <Route path="/profile/stats" element={<Stats />} />
+      <Route path="/profile/stats/subject/:subjectId" element={<SubjectStats />} />
+      <Route path="/profile/heatmap" element={<Heatmap />} />
+      <Route path="/profile/bookmarks" element={<Bookmarks />} />
+
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </HashRouter>
+</AuthGate>
+
       </TooltipProvider>
     </QueryClientProvider>
   );
