b178e03d2adaab42683c0788e36d91e79b6402ff
amahmoodi1379-glitch <a.mahmoodi1379@gmail.com>
2025-12-24 03:47:05 +0330

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import { themeStorage } from '@/lib/storage';
 import { initTelegramApp } from '@/lib/telegram';
 import AuthGate from "@/components/AuthGate";
@@ -21,7 +21,6 @@ import Stats from "./pages/Stats";
 import SubjectStats from "./pages/SubjectStats";
 import Heatmap from "./pages/Heatmap";
 import Bookmarks from "./pages/Bookmarks";
-import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -47,29 +46,29 @@ const App = () => {
         <Toaster />
         <Sonner position="top-center" />
         <AuthGate>
-  <HashRouter>
-    <Routes>
-      {/* Exam tab routes */}
-      <Route path="/" element={<ExamHome />} />
-      <Route path="/subjects/:subjectId/topics" element={<Topics />} />
-      <Route path="/topics/:topicId/subtopics" element={<Subtopics />} />
-      <Route path="/hub/:subtopicId" element={<SubtopicHub />} />
-      <Route path="/player" element={<Player />} />
-      <Route path="/summary" element={<SessionSummary />} />
+          <HashRouter>
+            <Routes>
+              {/* Exam tab routes */}
+              <Route path="/" element={<ExamHome />} />
+              <Route path="/subjects/:subjectId/topics" element={<Topics />} />
+              <Route path="/topics/:topicId/subtopics" element={<Subtopics />} />
+              <Route path="/hub/:subtopicId" element={<SubtopicHub />} />
+              <Route path="/player" element={<Player />} />
+              <Route path="/summary" element={<SessionSummary />} />
 
-      {/* Profile tab routes */}
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/profile/settings" element={<Settings />} />
-      <Route path="/profile/stats" element={<Stats />} />
-      <Route path="/profile/stats/subject/:subjectId" element={<SubjectStats />} />
-      <Route path="/profile/heatmap" element={<Heatmap />} />
-      <Route path="/profile/bookmarks" element={<Bookmarks />} />
+              {/* Profile tab routes */}
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/profile/settings" element={<Settings />} />
+              <Route path="/profile/stats" element={<Stats />} />
+              <Route path="/profile/stats/subject/:subjectId" element={<SubjectStats />} />
+              <Route path="/profile/heatmap" element={<Heatmap />} />
+              <Route path="/profile/bookmarks" element={<Bookmarks />} />
 
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </HashRouter>
-</AuthGate>
-
+              {/* Fix: Redirect any unknown routes (including Telegram launch params) to Home */}
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+          </HashRouter>
+        </AuthGate>
       </TooltipProvider>
     </QueryClientProvider>
   );
